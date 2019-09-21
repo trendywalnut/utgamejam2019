482e39a763bc873430ba6ade77358f32adc9faa6
Mason Eastman <masoneastman555@gmail.com>
2019-09-21 17:16:00 -0500

    Added rough StoryBoard

---
+++ Game Jam 2019/Assets/SceneSwitch.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneSwitch : MonoBehaviour
+{
+    private Animator myAnimator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myAnimator.Play("StoryBoard");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+ 
+    }
+}
