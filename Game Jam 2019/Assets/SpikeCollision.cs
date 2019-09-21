3f6b1f2b0e5ead3765b32a3327827dd37071830f
Steve Wang <swang3@cisco.com>
2019-09-21 14:28:44 -0500

    Remove cache

---
+++ Game Jam 2019/Assets/SpikeCollision.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpikeCollision : MonoBehaviour
+{
+    //public GameObject go;
+    //// Start is called before the first frame update
+    //void Start()
+    //{
+    //    go = GetComponent<>();
+    //}
+
+    // Update is called once per frame
+    //void Update()
+    //{
+    //    Debug.Log("trololol");
+    //}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "KirbyDummy")
+        {
+            Debug.Log("Lol");
+        } else
+        {
+            Debug.Log("haha");
+        }
+    }
+}
