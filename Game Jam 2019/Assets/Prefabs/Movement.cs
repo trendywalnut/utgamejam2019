1790cf3633a42b46fb35c288bcdb3dd7257d51be
Mason Eastman <masoneastman555@gmail.com>
2019-09-21 11:19:05 -0500

    Added updated prefab

--- Game Jam 2019/Assets/Prefabs/Movement.cs
+++ Game Jam 2019/Assets/Prefabs/Movement.cs
@@ -10,34 +10,49 @@ public class Movement : MonoBehaviour
     private bool jump;
     private Rigidbody2D myRigidBody;
     private Collider2D myCollider;
+    private SpriteRenderer mySpriteRenderer;
+    private Animator myAnimator;
 
     // Start is called before the first frame update
     void Start()
     {
         myRigidBody = GetComponent<Rigidbody2D>();
         myCollider = GetComponent<BoxCollider2D>();
+        mySpriteRenderer = GetComponent<SpriteRenderer>();
+        myAnimator = GetComponent<Animator>();
         jump = true;
     }
 
     // Update is called once per frame
     void Update()
     {
+        //if moving left
         if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
         {
+            //makes sprite face the right direction and plays animation
+            myAnimator.Play("PlayerRun");
+            mySpriteRenderer.flipX = false;
             myRigidBody.velocity = new Vector2(-mySpeed, myRigidBody.velocity.y);
         }
+        //if moving right
         if(Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
         {
+            //makes sprite face the right direction and plays animation
+            myAnimator.Play("PlayerRun");
+            mySpriteRenderer.flipX = true;
             myRigidBody.velocity = new Vector2(mySpeed, myRigidBody.velocity.y);
         }
-        if((Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.UpArrow)) && jump) {
+        //if jumping
+        if ((Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.UpArrow)) && jump) {
             myRigidBody.velocity = new Vector2(myRigidBody.velocity.x, myJumpForce);
             jump = false;
         }
     }
 
+    //after collision allows alien to jump again
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        myAnimator.Play("PlayerIdle");
         jump = true;
     }
 }
