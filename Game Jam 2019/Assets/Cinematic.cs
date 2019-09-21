127a5d456151963a04c3cfa418461749b02af075
Mason Eastman <masoneastman555@gmail.com>
2019-09-21 18:21:00 -0500

    Title screen/Cinematic

---
+++ Game Jam 2019/Assets/Cinematic.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Cinematic : MonoBehaviour
+{
+    private Animator myAnimator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myAnimator = GetComponent<Animator>();
+        myAnimator.Play("StoryBoard");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SceneManager.LoadScene(2);
+        }
+    }
+}
